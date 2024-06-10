9ac26b015b4a6586dfd6e473eef9b5ec4f761d25
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-11 00:49:17 +0200

    Home page - landing images update

--- src/components/home/landing-section.tsx
+++ src/components/home/landing-section.tsx
@@ -6,11 +6,11 @@ import ButtonPrimary from "@/components/buttons/button-primary";
 
 function LandingSection() {
     const images = [
-        'https://img.freepik.com/free-photo/heavy-machinery-used-construction-industry-engineering_23-2151307786.jpg?t=st=1718031043~exp=1718034643~hmac=5f95be98558787fa07bc9177cbf452c9a750d055b5e0391e86448068453c3917&w=2000',
-        'https://img.freepik.com/free-photo/photorealistic-stone-wall-surface_23-2151214790.jpg?t=st=1718031008~exp=1718034608~hmac=5d062f3b20e34761680165f4fe7844db70408980320873d8e86f50fa1a6cca1b&w=2000',
-        'https://img.freepik.com/free-photo/discussing-blueprint_1098-18180.jpg?t=st=1718031054~exp=1718034654~hmac=157abbb9df52efd3867612f73ef1c817bd5ddcf1dde98fad0ae7906172f20f1d&w=2000',
-        'https://img.freepik.com/free-photo/beautiful-black-white-upshot-new-york-city-subway-s-wtc-cortlandt-station-k-oculus_181624-1651.jpg?t=st=1718031048~exp=1718034648~hmac=426de53e9f9848533f75745f3427a1658fd22d5cfebb07a5e07ed6807f3d5999&w=2000',
-        'https://img.freepik.com/free-photo/construction-workers-sunset_53876-138180.jpg?t=st=1718031046~exp=1718034646~hmac=970e123b3332bb1088e541e0d73e89be2cfa6089a80849cc1b55acc3912fa14a&w=2000',
+            'https://images.pexels.com/photos/220885/pexels-photo-220885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/176342/pexels-photo-176342.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/159358/construction-site-build-construction-work-159358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/194096/pexels-photo-194096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            'https://images.pexels.com/photos/17687058/pexels-photo-17687058/free-photo-of-marble-steps-and-steel-handrail.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         ];
 
     return (
