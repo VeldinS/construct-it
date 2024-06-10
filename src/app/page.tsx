06b49c1b6529704c2ea828d0c4d2658222096ba2
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-10 18:42:40 +0200

    Home page - services cards section implementation and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
 import Navbar from "@/components/navbar/Navbar";
 import LandingSection from "@/components/home/landing-section";
+import ServiceCard from "@/components/home/service-card";
 
 export default function Home() {
     return (
         <div className={'w-screen h-auto bg-white flex flex-col items-center justify-center'}>
             <Navbar />
             <LandingSection />
+            <section className={'w-screen px-[5%] py-16 grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 grid-rows-2 gap-12'}>
+                <ServiceCard title={'Izrada kamene galanterije'}
+                             description={'Koristimo najkvalitetnije kamenje za kreiranje stolova, stolica, fontana i drugih elemenata.'}
+                             image={'/assets/icons/kamen-icon.svg'}
+                />
+                <ServiceCard title={'Opće građevinske usluge'}
+                             description={'Pružamo sve vrste građevinskih radova, od temelja do krova.'}
+                             image={'/assets/icons/grad-usluge-icon.svg'}
+                />
+                <ServiceCard title={'Asfaltiranje'}
+                             description={'Nudimo profesionalne usluge asfaltiranja prilaza, puteva i parkinga.'}
+                             image={'/assets/icons/asfaltiranje-icon.svg'}
+                />
+                <ServiceCard title={'Kamenorezački radovi'}
+                             description={'Specijalizirani smo za rezanje i obradu kamena za eksterijere i interijere.'}
+                             image={'/assets/icons/kamen-rez-icon.svg'}
+                />
+                <ServiceCard title={'Konsultacije'}
+                             description={'Naš tim stručnjaka nudi savjetodavne usluge za sve vaše građevinske projekte.'}
+                             image={'/assets/icons/konsultacije-icon.svg'}
+                />
+                <ServiceCard title={'Upravljanje projektima'}
+                             description={'Osiguravamo visoku kvalitetu upravljanja projektima za efikasnu realizaciju.'}
+                             image={'/assets/icons/upr-projektima-icon.svg'}
+                />
+            </section>
         </div>
     )
 }
