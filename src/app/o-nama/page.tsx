1689bf230a00a9675fe49de4c3137bb4c6a74406
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-13 18:00:24 +0200

    About page - testimonials section -  added

--- src/app/o-nama/page.tsx
+++ src/app/o-nama/page.tsx
@@ -4,6 +4,8 @@ import ProjectSection from "@/components/about/project-section";
 import ProjectsList from "@/components/about/projects-list";
 import VideoSection from "@/components/about/video-section";
 import FaqSection from "@/components/about/FAQ-section";
+import TestimonialsSection from "@/components/home/testimonials-section";
+import Footer from "@/components/footer/footer";
 
 function AboutPage() {
     return (
@@ -14,6 +16,7 @@ function AboutPage() {
             <ProjectsList />
             <VideoSection />
             <FaqSection />
+            <TestimonialsSection />
         </div>
     );
 }
